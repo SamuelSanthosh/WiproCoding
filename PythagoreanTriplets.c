017675abe42edbbc131f8a229d781d87b2a36439
SamuelSanthosh <35218854+SamuelSanthosh@users.noreply.github.com>
2018-09-09 12:06:49 +0530

    Create PythagoreanTriplets.c

---
+++ PythagoreanTriplets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void pythagoreanTriplets(int limit)
+{
+    int a,b,c=0,m=2;
+    while(c<limit)
+    {
+        for(int n=1;n<m;++n)
+        {
+            a=m*m-n*n;
+            b=2*m*n;
+            c=m*m+n*n;
+            if(c>limit)
+                break;
+            printf("%d %d %d\n",a,b,c);  
+        }    
+        m++;    
+    }
+}
+int main()
+{
+    int limit;
+    scanf("%d",&limit);
+    pythagoreanTriplets(limit);
+    return 0;
+}
